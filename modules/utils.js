0b8f59dc6235a35cf8e6d50fbfa237787a758f2d
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-09-10 22:55:10 -0400

    made utils for storing fields and value into db

--- modules/utils.js
+++ modules/utils.js
@@ -1,2 +1,27 @@
+var User = require('../models/user')
+
 // make function for modifying fields in mongo when a post request from frontend Node is called
-def modify mongo fields
+function modifyOrCreateFields (userEmail, query) {
+  User.findById(userEmail)
+    .exec(function (error, user) {
+      if (error) {
+        throw error
+      } else {
+        if (user === null) {
+          let err = new Error('User not found')
+          err.status = 400
+          throw err
+        } else {
+          let o = null
+          Object.keys(o).forEach(function (key) {
+            var val = o[key]
+            user.key = val
+          })
+        }
+      }
+    })
+}
+
+module.exports = {
+  modifyOrCreateFields
+}
